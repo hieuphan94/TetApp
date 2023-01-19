5c37242e202511a71857d29ec5bdbaaef520b853
Hiếu Phan <hida@Hieus-MacBook-Air.local>
2023-01-19 11:13:25 +0700

    Recone source

---
+++ js/web1.js
@@ -0,0 +1,7 @@
+console.log("asd");
+const clickToWeb2 = document.querySelector("._3stTEQ.imh8lg.IPnEEQ");
+
+clickToWeb2.addEventListener("click", function () {
+  console.log("sdas");
+  window.location.href = "https://www.google.com";
+});
